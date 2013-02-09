072d1d8b4ab6c7b328d475c3c9a4e0ae0b3a659f
Scott Basinger <sbasinge@gmail.com>
2013-02-09 15:31:54 -0500

    Remove anorm comments

--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,7 +1,5 @@
 package models
 
-//import anorm._
-//import anorm.SqlParser._
 import play.api.db._
 import play.api.Play.current
 import org.squeryl._
@@ -11,44 +9,7 @@ case class Task(id: Long, label: String) extends KeyedEntity[Long] {
 }
 
 object Task {
-  // Anorm Code
-  //	val task = {
-  //		get[Long]("id") ~
-  //		get[String]("label") map {
-  //			case id~label => Task(id, label)
-  //		}
-  //	}
-  //	
-  //	def all(): List[Task] = DB.withConnection { implicit c =>
-  //		SQL("select * from task").as(task *)
-  //	}
-  //
-  //	
-  //	def create(label:String) {
-  //		DB.withConnection { implicit c =>
-  //			SQL("insert into task (label) values ({label})").on(
-  //				'label -> label
-  //			).executeUpdate()
-  //		}
-  //	}
-  //	
-  //	def delete(id: Long) {
-  //		DB.withConnection { implicit c =>
-  //			SQL("delete from task where id = {id}").on(
-  //				'id -> id
-  //			).executeUpdate()
-  //		}
-  //	}
-
   //SqueryL Code
-    //	val task = {
-  //		get[Long]("id") ~
-  //		get[String]("label") map {
-  //			case id~label => Task(id, label)
-  //		}
-  //	}
-
-  
   def all: List[Task] = inTransaction(from(AppDB.tasks) { s => select(s) }.toList)
 
   def create(label:String) {
